275293ee73d78857b8ba647f9f5339885e50f963
Preethalakshmi K <preethalakshmi.kumaran@sky.uk>
2026-05-29 12:33:39 +0530

    added pw validation

--- backend/src/main/java/com/sr/mart/software/service/LoginService.java
+++ backend/src/main/java/com/sr/mart/software/service/LoginService.java
@@ -1,15 +1,13 @@
 package com.sr.mart.software.service;
 
 import com.sr.mart.software.entity.User;
-import com.sr.mart.software.enums.UserRoles;
 import com.sr.mart.software.model.LoginRequest;
 import com.sr.mart.software.model.LoginResponse;
 import com.sr.mart.software.repository.UserRepository;
 import com.sr.mart.software.responseMapper.LoginMapper;
-import java.util.List;
-import java.util.Optional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,12 +17,18 @@ public class LoginService {
 
     private LoginMapper loginMapper;
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public LoginResponse login(LoginRequest loginRequest) {
         User user = userRepository.findByUsername(loginRequest.getUsername()).orElseThrow(() ->
             new RuntimeException(
                 "Invalid username"
             ));
+
+        if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Incorrect password");
+        }
+
         return loginMapper.mapLoginRequestToLoginResponse(user);
     }
 
